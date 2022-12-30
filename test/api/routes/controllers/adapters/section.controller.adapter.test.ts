035b7636b8e106f51e3a22e0fa053430f5aeaff7
osilla <oscarsilla@outlook.com>
2022-12-30 12:44:58 +0100

    test: Section controller adapter

--- test/api/routes/controllers/adapters/section.controller.adapter.test.ts
+++ test/api/routes/controllers/adapters/section.controller.adapter.test.ts
@@ -1,13 +1,16 @@
-import { afterAll, beforeAll, describe, expect, it } from "@jest/globals";
+import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
 import { agent } from "supertest";
 import { HttpCode } from "../../../../../src/application/domain/http-code";
+import { Section } from "../../../../../src/application/domain/section";
 import { createServer } from "../../../../../src/boot";
 import { mongo } from "../../../../../src/infrastructure/database/mongo";
 
 const httpServer = createServer();
 const request = agent(httpServer);
 
-describe("GET /sections", () => {
+const section = new Section("Home", "Home description", "home.jpg", "");
+
+describe("/sections", () => {
   beforeAll(async () => {
     await mongo.createConnection();
   });
@@ -17,19 +20,48 @@ describe("GET /sections", () => {
     await mongo.closeConnection();
   });
 
-  it("should respond with a 404 status code when not found sections", async () => {
+  test("should respond with a 404 status code when not found sections", async () => {
     const response = await request.get("/api/v1/sections").send();
     expect(response.statusCode).toBe(HttpCode.NOT_FOUND);
   });
 
-  /* it("should respond with a 200 status code", async () => {
+  test("should respond with a 404 status code when not found specific section", async () => {
+    const response = await request.get("/api/v1/sections/home").send();
+    expect(response.statusCode).toBe(HttpCode.NOT_FOUND);
+  });
+
+  test("should respond with a 404 status code when try to modify an inexisting section", async () => {
+    section.setImg("nosection.jpg");
+    const response = await request
+      .patch("/api/v1/sections/nosection")
+      .send(section);
+    expect(response.statusCode).toBe(HttpCode.NOT_FOUND);
+    section.setImg("home.jpg");
+  });
+
+  test("should respond with a 201 status code when create a new section", async () => {
+    const response = await request.post("/api/v1/sections").send(section);
+    expect(response.statusCode).toBe(HttpCode.CREATED);
+  });
+
+  test("should response with a 200 status code when modify an existing section", async () => {
+    section.setImg("anotherImage.jpg");
+    const response = await request.patch("/api/v1/sections/home").send(section);
+    expect(response.statusCode).toBe(HttpCode.OK);
+  });
+
+  test("should respond with a 200 status code when find existing sections", async () => {
     const response = await request.get("/api/v1/sections").send();
     expect(response.statusCode).toBe(HttpCode.OK);
   });
 
-  it("should return a section list with at least one section", async () => {
-    const response = await request.get("/api/v1/sections");
-    console.log(response);
-    expect(response.body.length).toBeGreaterThan(0);
-  }); */
+  test("should respond with a 404 status code when try to delete an inexisting section", async () => {
+    const response = await request.delete("/api/v1/sections/nosection").send();
+    expect(response.statusCode).toBe(HttpCode.NOT_FOUND);
+  });
+
+  test("should respond with a 204 status code with delete an existing section", async () => {
+    const response = await request.delete("/api/v1/sections/home").send();
+    expect(response.statusCode).toBe(HttpCode.NO_CONTENT);
+  });
 });
