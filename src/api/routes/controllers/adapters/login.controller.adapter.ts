b4d4a3d479b95b63367016b023a62d50941c4b9e
osilla <oscarsilla@outlook.com>
2023-01-08 17:34:03 +0100

    feat: Auth login in progress

---
+++ src/api/routes/controllers/adapters/login.controller.adapter.ts
@@ -0,0 +1,7 @@
+import express, { NextFunction } from "express";
+const router = express.Router();
+
+router.post(
+  "/login",
+  async (req: Request, res: Response, next: NextFunction): Promise<> => {}
+);
