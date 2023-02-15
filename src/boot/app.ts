a8da9d0b65fac8db72063a0e2e58d53909010496
osilla <oscarsilla@outlook.com>
2023-02-15 20:14:58 +0100

    feat: Body size expanded to 10mb

--- src/boot/app.ts
+++ src/boot/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import errorHandler from "../api/middlewares/error-handler.middleware";
 import api from "../api/routes/api";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.json());
 
 app.use("/api/v1", api);
