4cc1beeec9e8a51a1b6e2b2f99b97b9b72802bb8
osilla <osilla@mercadona.es>
2023-02-26 17:53:06 +0100

    feat: Clear logs

--- src/infrastructure/repository/mongo/collections/events.collection.ts
+++ src/infrastructure/repository/mongo/collections/events.collection.ts
@@ -20,7 +20,6 @@ export class EventsCollection {
 
   async find(limit: number, page: number): Promise<EventModel[]> {
     const { mongo } = global.database;
-    console.log("limit: " + limit + " page: " + page);
     return await mongo
       .collection("events")
       .find({})
