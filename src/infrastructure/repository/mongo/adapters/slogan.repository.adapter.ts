d02da7ddfe6c4af8dbd2954084c438a77b9798ff
osilla <oscarsilla@outlook.com>
2023-03-08 22:19:32 +0100

    feat: SlogansCollection integrated

--- src/infrastructure/repository/mongo/adapters/slogan.repository.adapter.ts
+++ src/infrastructure/repository/mongo/adapters/slogan.repository.adapter.ts
@@ -2,10 +2,20 @@ import { SloganRepositoryPort } from "../../../../application/ports/out/slogan.r
 import { Slogan } from "../../../../application/domain/slogan";
 import { GeneratedId } from "../../../../application/domain/generated-id";
 import { SloganDaoMapper } from "../mappers/slogan.dao.mapper";
+import { SlogansCollection } from "../collections/slogans.collection";
+import { GeneratedIdDao } from "../models/generated-id.dao";
+import { GeneratedIdDaoMapper } from "../mappers/generated-id.dao.mapper";
 
 export class SloganRepositoryAdapter implements SloganRepositoryPort {
+  private slogansCollection: SlogansCollection = new SlogansCollection();
   private sloganDaoMapper: SloganDaoMapper = new SloganDaoMapper();
+  private generatedIdDaoMapper: GeneratedIdDaoMapper =
+    new GeneratedIdDaoMapper();
   async save(slogan: Slogan): Promise<GeneratedId> {
     const sloganDao = this.sloganDaoMapper.toSloganDao(slogan);
+    const generatedIdDao: GeneratedIdDao = await this.slogansCollection.save(
+      sloganDao
+    );
+    return this.generatedIdDaoMapper.toGeneratedId(generatedIdDao);
   }
 }
---
+++ src/infrastructure/repository/mongo/collections/slogans.collection.ts
@@ -0,0 +1,9 @@
+import { SloganDao } from "../models/slogan.dao";
+import { GeneratedIdDao } from "../models/generated-id.dao";
+
+export class SlogansCollection {
+  async save(sloganDao: SloganDao): Promise<GeneratedIdDao> {
+    const { mongo } = global.database;
+    return await mongo.collection("slogans").insertOne(sloganDao);
+  }
+}
