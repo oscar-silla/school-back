6acce75f088c47e28c9d32f6694b0a1c784c9b1c
osilla <osilla@mercadona.es>
2023-05-03 20:01:04 +0200

    refactor: Remove section

--- src/application/ports/in/services/section.service.port.ts
+++
@@ -1,9 +0,0 @@
-import { Section } from "../../../domain/section";
-
-export interface SectionServicePort {
-  createSection(section: Section): Promise<void>;
-  getSection(ref: string): Promise<Section>;
-  getSections(): Promise<Section[]>;
-  modifySection(ref: string, section: Section): Promise<void>;
-  deleteSection(ref: string): Promise<void>;
-}
--- src/application/ports/in/usecases/section/create-section.usecase.port.ts
+++
@@ -1,5 +0,0 @@
-import { Section } from "../../../../domain/section";
-
-export interface CreateSectionUseCasePort {
-  createSection(section: Section): Promise<void>;
-}
